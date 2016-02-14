e82b50feff43a38e730140a4a8f619b99fe6f9db
gomachan46 <shiro.gomachan46@gmail.com>
2016-02-14 12:24:24 +0900

    react-addons-test-utilを入れて雑に試しにApp.jsに対してテストを書いてみた

--- test/test.js
+++ test/test.js
@@ -1,5 +1,45 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import App from '../components/App'
 import assert from 'power-assert'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+import Footer from '../components/Footer'
 
-it ('is power-assert', () => {
-    assert('foo' === 'foo')
+function setup() {
+    let props = {}
+    let renderer = TestUtils.createRenderer()
+    renderer.render(<App {...props} />)
+    let output = renderer.getRenderOutput()
+
+    return {
+        props,
+        output,
+        renderer
+    }
+}
+
+describe("App", () => {
+    const { output } = setup()
+
+    it("outputとしてdivが返ってくること", () => {
+        assert(output.type === 'div')
+    })
+
+    describe("div内", () => {
+        const expected = output.props.children
+
+        it("要素は3つかえってくること", () => {
+            assert(expected.length === 3)
+        })
+        it("1番目の要素としてAddTodoが返ってくること", () => {
+            assert(expected[0].type === AddTodo)
+        })
+        it("2番目の要素としてVisibleTodoListが返ってくること", () => {
+            assert(expected[1].type === VisibleTodoList)
+        })
+        it("3番目の要素としてFooterが返ってくること", () => {
+            assert(expected[2].type === Footer)
+        })
+    })
 })
